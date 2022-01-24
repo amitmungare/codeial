b44ec4a120b66a073b9ce31cb2e1eef4a06c20dd
amitmungare <amungare27@gmail.com>
2022-01-24 23:49:26 +0530

    Manual Authentication

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+
+
+db.once('open', function(){
+    console.log('Connected to db');
+});
+
+
+module.exports = db;
